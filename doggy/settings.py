6f35077c149269a84cec070798bc7c8743a010f2
zeuspod <josseluism@gmail.com>
2022-08-08 15:27:29 -0400

    home page add

--- doggy/settings.py
+++ doggy/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'doggy.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['*'],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -78,8 +78,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'doggy',
-        'USER': 'renesilva',
-        'PASSWORD': 'renesilva',
+        'USER': 'josemorales',
+        'PASSWORD': 'jose1234',
         'HOST': 'localhost',
         'PORT': '',
          
@@ -121,7 +121,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
