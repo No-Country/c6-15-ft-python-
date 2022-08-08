3dcf5bdc0abe22a7f3f3ff973809e8dc76810bae
sunflower <rasp17698@gmail.com>
2022-08-08 15:01:43 -0500

    set de base de datos

--- doggy/settings.py
+++ doggy/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'doggy.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'dogy',
+        'USER': 'postgres',
+        'PASSWORD': '187984',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
