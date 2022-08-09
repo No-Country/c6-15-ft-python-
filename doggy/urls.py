52c7b8e9529e7c2acf8b5d0ffa5f1e9620cf5f76
pamasime790 <rene.scre@gmail.com>
2022-08-08 19:41:53 -0500

    Cree una nueva aplicacion y en esa cree el login

--- doggy/urls.py
+++ doggy/urls.py
@@ -16,8 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from homepage import urls
+from useraccount import urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('homepage.urls')),
+    path('useraccount/', include('useraccount.urls')),
+
 ]
