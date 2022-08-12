75ca9542dae60b2e12c6c9d0d2afb2d32ac454ad
zeuspod <josseluism@gmail.com>
2022-08-12 11:53:42 -0400

    app sitter add

--- doggy/settings.py
+++ doggy/settings.py
@@ -36,6 +36,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'useraccount',
     'homepage',
+    'sitter',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
