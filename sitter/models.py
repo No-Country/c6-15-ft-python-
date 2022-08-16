cca24f2f3e3595316f585012e0b651337c7a8c96
zeuspod <josseluism@gmail.com>
2022-08-16 14:35:04 -0400

    fix model sitters

--- sitter/models.py
+++ sitter/models.py
@@ -1,19 +1,20 @@
 from django.db import models
-from homepage.models import User
+from django.contrib.auth.models import User
+
 
 # Create your models here.
 class Sitter(models.Model):
     price = models.IntegerField()
-    descripcion = models.CharField(max_length=200)
+    descripcion = models.CharField(max_length=255)
     status = models.BooleanField(default=True)
     picture_site = models.ImageField(upload_to='sitter/img/') 
     #user relationship
-    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    sitter_id = models.ForeignKey(User, on_delete=models.CASCADE)
     
 
 
     def __str__(self):
-        return self.name.Sitter
+        return self.sitter_id.username
 
     
     class meta:
