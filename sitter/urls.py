cc2f2ca264431ca2da562d50723cc038e78f7a83
sunflower <rasp17698@gmail.com>
2022-08-18 13:13:07 -0500

    test sitter fk

--- sitter/urls.py
+++ sitter/urls.py
@@ -2,5 +2,5 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.create_sitter, name='create_sitter'),
+    path('<str:user>', views.create_sitter, name='create_sitter'),
 ]
--- sitter/views.py
+++ sitter/views.py
@@ -1,12 +1,16 @@
 from django.shortcuts import render
 from sitter.forms import SitterForm
+from django.contrib.auth.models import User
 # Create your views here.
 
 
-def create_sitter(request):
+def create_sitter(request,user):
     if request.user.is_authenticated:
         form = SitterForm(request.POST or None)
         if form.is_valid():
+            formulario = form.save(commit=False)
+            user = User.objects.get(username = request.user.username)
+            formulario.user_id = user
             form.save()
         context = {
             'form': form,
