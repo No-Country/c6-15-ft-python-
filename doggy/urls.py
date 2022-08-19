dd39962952b048be980ee7662f8a7375c5bd7b3a
rene3255 <ranchoeltecal.com>
2022-08-19 16:38:05 -0500

    fixes

--- doggy/urls.py
+++ doggy/urls.py
@@ -17,6 +17,8 @@ from django.contrib import admin
 from django.urls import path, include
 from homepage import urls
 from useraccount import urls
+from pets import urls
+from sitter import urls
 from django.conf import settings
 from django.conf.urls.static import static
 
--- useraccount/urls.py
+++ useraccount/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path, include
 from . import views
-
 urlpatterns = [
     path('login',views.login_doggy, name='login'),
     path('register',views.register,name='register'),
-    path('logout',views.logout_doggy,name='logout'),
+    path('logoute',views.logout_doggy,name='logout'),
     path('edit_profile',views.edit_profile, name='edit_profile'),
 ]
