43366cefc5ac6df508ae7efaba8742a1833724ad
sunflower <rasp17698@gmail.com>
2022-08-18 20:40:12 -0500

    add: order to admin site

--- sitter/admin.py
+++ sitter/admin.py
@@ -1,5 +1,14 @@
 from django.contrib import admin
 from .models import Sitter
 # Register your models here.
-admin.site.register(Sitter)
+
+
+class SitterAdmin(admin.ModelAdmin):
+    list_display = ( 'user_id','price', 'descripcion','status', 'picture_site')
+    ordering = ('user_id',)
+    search_fields = ('user_id__username','price','description','status')
+
+
+
+admin.site.register(Sitter, SitterAdmin)
 
