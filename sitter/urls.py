d3b7b8e3c6c00d2249849635b4260daa111ca7d2
sunflower <rasp17698@gmail.com>
2022-08-23 09:59:01 -0500

    add: list sitter publications & styles

--- sitter/urls.py
+++ sitter/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path, include
 from . import views
 
+
+
 urlpatterns = [
     path('<str:user>', views.create_sitter, name='create_sitter'),
+    path('find_publication/',views.sitter, name='sitter_list'),
 ]
