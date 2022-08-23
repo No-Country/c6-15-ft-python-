877ee801c4389fd5bcbced045fba91aaf8f72c93
zeuspod <josseluism@gmail.com>
2022-08-23 18:25:48 -0400

    placeholder checkou fixed

--- reservations/forms.py
+++ reservations/forms.py
@@ -9,7 +9,7 @@ today = date.today()
 class ReservationsForm(forms.ModelForm):
 
      check_in = forms.DateField(widget=forms.TextInput(attrs={'min': today, 'value': today ,'placeholder': 'Check In', 'type': 'date' }))
-     check_out = forms.DateField(widget=forms.TextInput(attrs={'min': today, 'value': today ,'placeholder': 'Check In', 'type': 'date' }))
+     check_out = forms.DateField(widget=forms.TextInput(attrs={'min': today, 'value': today ,'placeholder': 'Check Out', 'type': 'date' }))
 
      class Meta:
         model = Reservations
