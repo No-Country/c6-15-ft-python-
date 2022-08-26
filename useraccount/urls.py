802f7e234fcdf331eb39d9c009fd8fd789b700da
pamasime790 <rene.scre@gmail.com>
2022-08-26 10:16:32 -0500

    Urls

--- useraccount/urls.py
+++ useraccount/urls.py
@@ -14,7 +14,11 @@ urlpatterns = [
     path('editpro', UserEditView.as_view(), name='editpro'),
 
     path('password', PwdChangeView.as_view(template_name='registration/password.html'), name='password'),
+<<<<<<< HEAD
     #path('password_success', views.password_success, name='password_success'),
+=======
+   # path('password_success', views.password_success, name='password_success'),
+>>>>>>> Urls
     path('editprofile/<int:pk>/', UserEditExtendedView.as_view(), name='editprofile'), 
    # path('create_profile', views.create_profile, name='create_profile'),
 
