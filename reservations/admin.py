5bbcdb4a599e70039de3275ecb859ea74c9df054
sunflower <rasp17698@gmail.com>
2022-08-30 14:33:33 -0500

    fix: model reservations

--- reservations/admin.py
+++ reservations/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
-from .models import Reservation
+from .models import Reservations
 # Register your models here.
 
 class reservationsAdmin(admin.ModelAdmin):
     list_display = ('check_in', 'check_out', 'sitter_publication', 'user_id', 'payment_type', 'status')
 
-admin.site.register(Reservation)
+admin.site.register(Reservations)
