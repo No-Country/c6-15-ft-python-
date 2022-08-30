9eeff5467dda939e3c1aacb91f1553237697fd07
pamasime790 <rene.scre@gmail.com>
2022-08-30 14:15:54 -0500

    reservation2

--- reservations/admin.py
+++ reservations/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
-from reservations.models import Reservations
+from reservations.models import Reservation
 # Register your models here.
 
 class reservationsAdmin(admin.ModelAdmin):
     list_display = ('check_in', 'check_out', 'sitter_publication', 'user_id', 'payment_type', 'status')
 
-admin.site.register(Reservations)
+admin.site.register(Reservation)
