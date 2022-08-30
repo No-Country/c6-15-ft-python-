87eb0012cdf465545933421dc206ddd84d18e765
pamasime790 <rene.scre@gmail.com>
2022-08-30 14:01:26 -0500

    reservations

--- reservations/models.py
+++ reservations/models.py
@@ -19,13 +19,13 @@ status_reservations = [
 
 
 class Reservations(models.Model):
+    sitter_publication = models.ForeignKey(Sitter, on_delete=models.CASCADE)
+    payment_type = models.IntegerField(null=False, blank=False, choices=payments_types , default=1)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
     reservation_date = models.DateField(auto_now_add=True)
     check_in = models.DateField()
     check_out = models.DateField()
-    payment_type = models.IntegerField(null=False, blank=False, choices=payments_types , default=1)
     status = models.IntegerField(null=False, blank=False, choices=status_reservations , default=2)
-    sitter_publication = models.ForeignKey(Sitter, on_delete=models.CASCADE)
-    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
 
 
     def __str__(self):
